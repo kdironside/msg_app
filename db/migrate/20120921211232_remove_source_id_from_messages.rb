3d6647a7d9793c3479fb2cd1ba17ff7d8e916c27
KD Ironside <kdironside@gmail.com>
2012-09-22 22:05:58 -0500

    removed extra migration file

--- db/migrate/20120921211232_remove_source_id_from_messages.rb
+++
@@ -1,9 +0,0 @@
-class RemoveSourceIdFromMessages < ActiveRecord::Migration
-  def up
-    remove_column :messages, :source_id
-  end
-
-  def down
-    add_column :messages, :source_id, :integer
-  end
-end
