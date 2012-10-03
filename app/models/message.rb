3fb0f0ad6f7548eebdd80b90e69f26bc9e3d741e
KD Ironside <kdironside@gmail.com>
2012-10-03 15:34:30 -0500

    add message validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,17 @@
 class Message < ActiveRecord::Base
+  before_validation :strip_msg_text_whitespace
+  
   attr_accessible :msg_text, :read, :user_id
   
   belongs_to :conversation
+  
+  validates_presence_of :msg_text
+  
+private
+  
+  def strip_msg_text_whitespace
+    self.msg_text.strip!
+  end
+  
   
 end
