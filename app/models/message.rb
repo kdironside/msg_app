b0d06520087a12a1d027e411b67ed67b8a6a0d41
KD Ironside <kdironside@gmail.com>
2012-10-03 15:06:04 -0500

    removed message, user association

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,6 @@
 class Message < ActiveRecord::Base
   attr_accessible :msg_text, :read, :user_id
   
-  belongs_to :conversation 
-  belongs_to :user, :through => :conversation
+  belongs_to :conversation
   
 end
